b5d01d042ff97248c3bfd66a747ee9a01189c235
Andrei Kavalenka <andrew.kovalenko65@gmail.com>
2023-10-14 13:07:27 +0200

    Count vectorizer matrix

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,42 @@
 use polars::prelude::*;
 use linfa_preprocessing::*;
+use ndarray::Array;
 
 
 fn main() {
-    let _df = CsvReader::from_path("data/train_data.csv").unwrap().finish().unwrap();
-    let _df = _df.select(["snippet", "language"]).unwrap();
+    // Data reading
+    let df = CsvReader::from_path("data/train_data_5exp.csv")
+        .unwrap()
+        .finish()
+        .unwrap();
+    // Selecting ["snippet", "language"] columns
+    let df = df.select(["snippet", "language"])
+        .unwrap();
 
-    // println!("{}", _df.head(Some(3)));
+    println!("Dataframe shape: {:?}.", df.shape());
 
-    // This doesn't work, because to_ndarray doesn't convert strings.
-    println!("{:?}", _df.to_ndarray::<Float64Type>(Default::default()).unwrap());
+    // Get vector of snippet df column
+    let snippets: Vec<String> = df.column("snippet")
+        .unwrap()
+        .utf8()
+        .unwrap()
+        .into_iter()
+        .map(|opt_s| opt_s.unwrap_or_default().to_string())
+        .collect();
 
-    // let x_train_counts = CountVectorizer::params()
-    //     .n_gram_range(1,1)
-    //     .fit(&_df.select(["snippet"])
-    //         .unwrap()
-    //         .to_ndarray::<Float64Type>(Default::default())
-    //         .unwrap())
-    //     .unwrap();
-    //     // .transform(&_df.select(["snippet"]).unwrap());
-    // println!("{}", x_train_counts.head(Some(3)));
+
+    // Convert Vec<String> to ArrayBase
+    let snippets_array = Array::from(snippets);
+
+    // Now use the array for CountVectorizer
+    let count_vectorizer = CountVectorizer::params()
+        .n_gram_range(1, 1)
+        .fit(&snippets_array)
+        .unwrap();
+
+    // Transform snippets_array by count_vectorizer
+    let training_matrix = count_vectorizer
+        .transform(&snippets_array);
+
+    println!("{:?}", training_matrix.shape())
 }
